c23efbdba32b62ac931ad97c7483a406d7508703
Sir Angel Naguit <angelnaguit@icloud.com>
2019-10-29 13:58:59 -0400

    Added Route
    plus Strings xml for rates

---
+++ app/src/main/java/ca/georgebrown/comp3074/positiontracker/AddedRoute.java
@@ -0,0 +1,14 @@
+package ca.georgebrown.comp3074.positiontracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddedRoute extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_added_route);
+    }
+}
