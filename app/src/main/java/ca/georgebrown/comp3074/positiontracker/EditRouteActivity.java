c7603af91c78a484befedfdc4363f843e0fb181b
lilianyangc <mlilianyang@gmail.com>
2019-11-14 12:55:13 -0500

    Edit Route Activity code added

--- app/src/main/java/ca/georgebrown/comp3074/positiontracker/EditRouteActivity.java
+++ app/src/main/java/ca/georgebrown/comp3074/positiontracker/EditRouteActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import ca.georgebrown.comp3074.positiontracker.model.Route;
+import ca.georgebrown.comp3074.positiontracker.sql.DbHelper;
 
 public class EditRouteActivity extends AppCompatActivity {
 
@@ -17,12 +21,36 @@ public class EditRouteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_route);
 
+        final DbHelper dbHelper = new DbHelper(this);
+
+        final EditText name = findViewById(R.id.editName);
+        final Spinner rate = findViewById(R.id.spinRatings);
+        final EditText tags = findViewById(R.id.editTags);
+
         final Route route = (Route)getIntent().getExtras().getSerializable("route");
+        String myString = String.valueOf(route.getRating()); //the value you want the position for
+        ArrayAdapter myAdap = (ArrayAdapter) rate.getAdapter(); //cast to an ArrayAdapter
+        int spinnerPosition = myAdap.getPosition(myString);
+
+        name.setText(route.getRouteName());
+        rate.setSelection(spinnerPosition); //set the default according to value
+//        tags.setText(route.getTags());
 
         Button saveBtn = findViewById(R.id.btnSave);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String rName = name.getText().toString();
+//              String rTag = rate.getText().toString();
+                int rRate = Integer.parseInt(rate.getSelectedItem().toString());
+
+                route.setRouteName(rName);
+//              route.setTags(tags);
+                route.setRating(rRate);
+
+                dbHelper.updateRoute(route);
+
                 Toast t = Toast.makeText(view.getContext(),"Successfully updated route!",Toast.LENGTH_LONG);
                 t.show();
                 finish();
