d58fecf6dc730420883af85f983236c864fcb70a
lilianyangc <mlilianyang@gmail.com>
2019-11-17 13:49:49 -0500

    Marker added to MapsActivity

--- app/src/main/java/ca/georgebrown/comp3074/positiontracker/MapsActivity.java
+++ app/src/main/java/ca/georgebrown/comp3074/positiontracker/MapsActivity.java
@@ -9,7 +9,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -44,21 +46,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+         LatLng latLng = new LatLng(51.432739, -0.523061);
 
-//        Coordinates newCoor = new Coordinates();
+        //Coordinates newCoor = new Coordinates();
 
-        PolylineOptions polylineOptions = new PolylineOptions();
-        polylineOptions.add(new LatLng(51.432739, -0.523061));
-        polylineOptions.add(new LatLng(51.429805, -0.549850));
-        polylineOptions.add(new LatLng( 51.425900, -0.560324));
-        polylineOptions.add(new LatLng( 51.436070, -0.566330));
+        //Adding coordinates to a line
+        PolylineOptions line = new PolylineOptions();
+        line.add(new LatLng(51.432739, -0.523061));
+        line.add(new LatLng(51.429805, -0.549850));
+        line.add(new LatLng( 51.425900, -0.560324));
+        line.add(new LatLng( 51.436070, -0.566330));
 
 
-        polylineOptions.width(5).color(Color.RED);
-        mMap.addPolyline(polylineOptions);
+        line.width(5).color(Color.RED);
+        mMap.addPolyline(line);
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.432739, -0.523061),12));
 
+        Marker mStart = mMap.addMarker(new MarkerOptions()
+                .position(latLng).title("Start")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mStart.setTag(0);
+
+        Marker mEnd = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng( 51.436070, -0.566330)).title("End")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        mEnd.setTag(0);
 //         for(Coordinates c : coordinates){
 //            System.out.println(c.getLatitude() + "\t " + c.getLongitude());
 //        }
